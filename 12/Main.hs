43376bb9c379ae80c8179f38a03db8533e165c98
Aner Lucero <4rgento@gmail.com>
2017-12-12 12:08:33 -0300

    Day 12

--- 12/Main.hs
+++ 12/Main.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE TupleSections #-}
 module Main where
 
-import Data.Graph
+import Data.Graph as G
+import Data.Tree as T
 
-import Debug.Trace (trace)
+import qualified Data.List as L
 
-type Id = Integer
+type Id = Int
 data Link = Link Id [Id] deriving Show
+
+_root :: Link -> Id
+_root (Link id _) = id
 
 parseLink :: String -> Either String Link
 parseLink str = case words str of
@@ -16,8 +21,21 @@ parseLink str = case words str of
 parseInput :: String -> Either String [Link]
 parseInput input = mapM parseLink (lines input)
 
+linkToEdges :: Link -> [G.Edge]
+linkToEdges (Link root children) = map (root,) children
+
+graph :: [Link] -> G.Graph
+graph links = G.buildG (minid,maxid) $ concatMap linkToEdges links
+  where
+  maxid = L.maximum $ map _root links
+  minid = L.minimum $ map _root links
+
 main :: IO ()
 main =
   -- (parseLink <$> readFile "input.txt") >>=
   readFile "input.txt" >>= return . parseInput >>=
-  print
+  --print . (map (T.foldTree folder ) . filter ((==0) . T.rootLabel) . G.components . graph <$>)
+  print . (length . G.components . graph <$>)
+  where
+  folder :: G.Vertex -> [Int] -> Int
+  folder _ = (+1) . sum
